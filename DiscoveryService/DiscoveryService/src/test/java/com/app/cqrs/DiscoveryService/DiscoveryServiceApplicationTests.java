516d17fa7ab8dc30ea89e628e2f07ce4712d39ce
Tasfequr Rahman <tasfeq123@gmail.com>
2022-06-07 04:08:53 +0200

    Initial commit

---
+++ DiscoveryService/DiscoveryService/src/test/java/com/app/cqrs/DiscoveryService/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.cqrs.DiscoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
